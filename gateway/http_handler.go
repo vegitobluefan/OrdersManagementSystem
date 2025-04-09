fb67fedc0cd90f0942be8539437adb26442584d3
Daniyar <daniararinov995@gmail.com>
2025-04-10 02:59:54 +0300

    добавил json, проработал действия пользователей

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,13 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	common "github.com/vegitobluefan/OrdersManagementSystem-commons"
+	pb "github.com/vegitobluefan/OrdersManagementSystem-commons/api"
+)
 
 type handler struct {
-	// gateway
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{client}
 }
 
 func (h *handler) registerRoutes(mux *http.ServeMux) {
@@ -15,5 +20,16 @@ func (h *handler) registerRoutes(mux *http.ServeMux) {
 }
 
 func (h *handler) HanleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	customerID := r.PathValue("customerID")
 
+	var items []*pb.ItemsWithQuantity
+	if err := common.ReadJSON(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerID,
+		Items:      items,
+	})
 }
--- gateway/main.go
+++ gateway/main.go
@@ -6,15 +6,29 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	common "github.com/vegitobluefan/OrdersManagementSystem-commons"
+	pb "github.com/vegitobluefan/OrdersManagementSystem-commons/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr          = common.EnvString("HTTP_ADDR", ":8080")
+	ordersServiceAddr = "localhost:3000"
 )
 
 func main() {
+	conn, err := grpc.Dial(ordersServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Ошибка подключения к серверу: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Начинаем сервис заказов на ", ordersServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Запуск http сервера на %s", httpAddr)
